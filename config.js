68a38ca7bb51fd85d07a413a3fea0b1579a68d28
iwamizawa-software <iwamizawasoftware@icloud.com>
2022-06-24 23:26:44 +0900

    configテスト

---
+++ config.js
@@ -0,0 +1,16 @@
+{
+  takeStreamImmediately : 1,
+  numbering : 1,
+  clearBubble: 1,
+  notifyMention: 1,
+  replyMsg: 'ｎ',
+  notifyAccess: 1,
+  accessLog: 0,
+  autoBlock: '',
+  logWindowCSS: `
+.message {
+padding: 2px 0;
+border-bottom: 1px solid #000;
+}
+  `,
+}
