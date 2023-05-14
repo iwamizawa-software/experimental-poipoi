da5d2e23357cf067605e672c87772119bf81742b
iwamizawa-software <iwamizawasoftware@icloud.com>
2023-05-14 22:41:51 +0900

    modify json ext

--- i18n.js
+++ i18n.js
@@ -1,6 +1,6 @@
 var messages = {};
 await Promise.all(vueApp._container._vnode.component.proxy.$i18n.availableLocales.map(async l => {
-  Object.keys(messages[l] = eval('(' + await (await fetch('https://raw.githubusercontent.com/iccanobif/gikopoi2/master/src/langs/' + l + '.json')).text() + ')')).forEach(k => {
+  Object.keys(messages[l] = eval('(' + await (await fetch('https://raw.githubusercontent.com/iccanobif/gikopoi2/master/src/langs/' + l + '.json5')).text() + ')')).forEach(k => {
     if (k !== 'room' && k !== 'default_user_name')
       delete messages[l][k];
   });
